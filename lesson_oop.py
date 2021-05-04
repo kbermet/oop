dd236c3d15456c739811066cb79686e6c1bd5949
kbermet <kbermet@gmail.com>
2021-05-04 19:30:34 +0600

    initial commit

---
+++ lesson_oop.py
@@ -0,0 +1,20 @@
+class Phone:
+
+    name = "android"
+    make = "samsung"
+    model = 2012
+
+
+    def selfie(self):
+        print("Фотографии")
+
+    def settings(self):
+        print("Настройки")
+
+
+phone_1 = Phone()
+
+# Создаем объект класса Car под названием car_b
+phone_2 = Phone()
+
+print(phone_1.name)
